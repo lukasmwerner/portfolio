4f8da8bf4767dd42275343c01e8ff9043a81b926
Lukas Werner <55150634+lwerner-lshigh@users.noreply.github.com>
2022-02-03 20:52:08 -0800

    update the gallery parameters

--- src/Photos.svelte
+++ src/Photos.svelte
@@ -4,17 +4,17 @@
     const images = [
         { src: '/gallery/_DSC0028-2.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC0047.jpg', width: 600, height: 400 },
-        { src: '/gallery/_DSC0179-2.jpg', width: 400, height: 600 },
+        /*{ src: '/gallery/_DSC0179-2.jpg', width: 400, height: 600 },*/
         { src: '/gallery/_DSC0238-2.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC0315-2.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC0437.jpg', width: 600, height: 400 },
-        { src: '/gallery/_DSC0525-2.jpg', width: 600, height: 400 },
+        /*{ src: '/gallery/_DSC0525-2.jpg', width: 600, height: 400 },*/
         { src: '/gallery/_DSC0767.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC0787.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC1369-2.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC7216.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC8045.jpg', width: 600, height: 400 },
-        { src: '/gallery/_DSC8244.jpg', width: 600, height: 400 },
+        { src: '/gallery/_DSC8244.jpg', width: 1100, height: 400 },
         { src: '/gallery/_DSC8513.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC8612.jpg', width: 600, height: 400 },
         { src: '/gallery/_DSC8699.jpg', width: 600, height: 400 },
@@ -22,9 +22,9 @@
     ];
 </script>
 
-<div class="healthyspace">
+<div class="healthyspace" id="photos">
     <h1>Photos</h1>
-    <Gallery images={images} />
+    <Gallery images={images} rowHeight={300} />
 </div>
 
 <style>
